2e93ca3fe0cac9af35a65e0088eca6e3e3a2caa5
Hugo Kawamata <hugokawamata@gmail.com>
2017-06-28 23:44:13 +1000

    Flip complete

--- src/Components/Panel.js
+++ src/Components/Panel.js
@@ -9,14 +9,13 @@ export default class Panel extends React.Component {
         this.state = {
             column: props.col, // Out of 24 (or 12 if mobile)
             row: props.row, // Out of 12 (or 24 if mobile)
-            delay: (props.row * 2 + props.col) * 80,
+            delay: (props.row * 2 + props.col) * 120,
             className: "panel-comp panel-unflipped"
         }
         setTimeout(() => this.flipSelf(), 1000+this.state.delay);
     }
 
     flipSelf() {
-        console.log("cuckme" + this.state.column + " " + this.state.row)
         this.setState(
             {
                 column: this.state.column,
@@ -25,7 +24,20 @@ export default class Panel extends React.Component {
                 className: "panel-comp panel-flipping"
             }
         );
+        setTimeout(() => this.finishFlip(), 500)
 
+    }
+
+    finishFlip() {
+        console.log("to");
+        this.setState(
+            {
+                column: this.state.column,
+                row: this.state.row,
+                delay: this.state.delay,
+                className: "panel-comp panel-flipped"
+            }
+        )
     }
 
     render() {
