b1ce0cf967ba6f6d1f645877645224fba419f09e
Hugo Kawamata <hugokawamata@gmail.com>
2021-06-17 16:58:16 +1000

    ✨ Add redux to app

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,42 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux"
+import { changeLanguage } from "../store/config"
 import { withRouter, Link } from 'react-router-dom';
 
 const NavbarWR = withRouter(props => <Navbar {...props}/>);
 export default NavbarWR;
 
-class Navbar extends React.Component {
-    render() {
-        return (
-            <div className="nav-container">
-                <nav className="nav">
-                    <div className="nav-left">
-                        <Link to="/" className={this.props.location.pathname === "/" ? "nav-item nav-current" : "nav-item"}>
-                            About
-                        </Link>
-                        <Link to="/projects" className={this.props.location.pathname === "/projects" ? "nav-item nav-current" : "nav-item"}>
-                            Projects
-                        </Link>
-                    </div>
-                    <div className="nav-center">
-                        <a className="nav-item">
-                            <div className="logo-container">
-                                <img className="logo" src="images/logo.png" alt="HK logo"/>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="nav-right">
-                        <Link to="/personal" className={this.props.location.pathname === "/personal" ? "nav-item nav-current" : "nav-item"}>
-                            Personal
-                        </Link>
-                        <a href="https://www.issei.com.au" className="nav-item">
-                            Issei
-                        </a>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+export function Navbar (props) {
+  const language = useSelector((state) => state.config.language)
+  const dispatch = useDispatch()
+
+  return (
+    <div className="nav-container">
+      <nav className="nav">
+        <div className="nav-left">
+          <Link to="/" className={props.location.pathname === "/" ? "nav-item nav-current" : "nav-item"}>
+            About
+          </Link>
+          <Link to="/projects" className={props.location.pathname === "/projects" ? "nav-item nav-current" : "nav-item"}>
+            Projects
+          </Link>
+        </div>
+          <div className="nav-center">
+            <a className="nav-item" onClick={() => dispatch(changeLanguage("ja"))}>
+              <div className="logo-container">
+                <img className="logo" src="images/logo.png" alt="HK logo"/>
+              </div>
+            </a>
+          </div>
+          <div className="nav-right">
+            <Link to="/personal" className={props.location.pathname === "/personal" ? "nav-item nav-current" : "nav-item"}>
+              Personal
+            </Link>
+            <a href="https://www.issei.com.au" className="nav-item">
+              Issei
+            </a>
+          </div>
+      </nav>
+    </div>
+  );
 }
